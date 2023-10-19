f5d19372d593116f9ea40266c12fad38f69b4efd
SaidouJalloh <saidhw95@gmail.com>
2023-10-19 16:52:11 +0000

    add modif

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import cv2
+import io
 import numpy as np
+from PIL import Image
 from keras.models import load_model
 
 # Charger le modèle
@@ -14,38 +15,45 @@ class_names = {
     4: "Healthy"
 }
 
-st.title("Prediction de l'etat des feuilles de manioc cassava")
+# Fonction pour prédire l'état des feuilles de manioc
+def predict(image_file):
+    # Charger l'image en utilisant Pillow (PIL)
+    image = Image.open(image_file)
+    image = np.array(image)
+
+    # Redimensionner l'image à la taille attendue par le modèle (224x224)
+    image = np.array(Image.fromarray(image).resize((224, 224)))
+
+    # Prétraiter l'image (par exemple, normalisation)
+    image = image / 255.0  # Normalisation (vous pouvez adapter le prétraitement en fonction de votre modèle)
+
+    # Effectuer la prédiction
+    prediction = model.predict(np.expand_dims(image, axis=0))
+
+    # Obtenir la classe prédite
+    predicted_class = np.argmax(prediction)
+    predicted_class_name = class_names[predicted_class]
+
+    return predicted_class, predicted_class_name
+
+# Titre de l'application
+st.title("Prédiction de l'état des feuilles de manioc")
+
+# Description de l'application
+st.markdown("Cette application utilise un modèle d'apprentissage profond pour prédire l'état des feuilles de manioc.")
 
 # Charger l'image depuis l'ordinateur local
 file_to_predict = st.file_uploader("Télécharger une image", type=["jpg", "png", "jpeg"])
 if file_to_predict is not None:
-    image_bytes = file_to_predict.read()
-    image_array = np.frombuffer(image_bytes, np.uint8)
-    image_to_predict = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    # Obtenez les prédictions
+    predicted_class, predicted_class_name = predict(file_to_predict)
 
-    if image_to_predict is not None:
-        st.image(image_to_predict, caption="Image chargée", use_column_width=True)
-        
-        # Redimensionnez l'image à la taille attendue par le modèle (224x224)
-        target_size = (224, 224)
-        image_to_predict = cv2.resize(image_to_predict, target_size)
-        
-        # Normalisez l'image si nécessaire
-        image_to_predict = image_to_predict / 255.0
-        
-        # Utilisez le modèle pour obtenir les prédictions
-        img_to_predict = np.expand_dims(image_to_predict, axis=0)
-        predictions = model.predict(img_to_predict)
-        
-        # Obtenez la classe prédite (indice de la classe)
-        predicted_class = np.argmax(predictions)
-        predicted_class_name = class_names.get(predicted_class, "Classe inconnue")
-        
+    if predicted_class is not None:
+        st.image(file_to_predict, caption="Image chargée", use_column_width=True)
+
         # Affichez la classe prédite
-        st.write(f"Predicted Class: {predicted_class} - {predicted_class_name}")
-        
-        # Affichez les probabilités pour chaque classe
-        st.write("Probabilités :", predictions)
+        st.write(f"Classe prédite : {predicted_class} - {predicted_class_name}")
     else:
         st.write("Erreur de chargement de l'image.")
-
+else:
+    st.write("Veuillez télécharger une image.")
